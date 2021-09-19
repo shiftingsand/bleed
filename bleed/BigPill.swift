ff0b924d7ac639c307a90dab67dc2aa191f25ac7
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2021-09-19 18:12:23 -0400

    Added individual elements.

--- bleed/BigPill.swift
+++ bleed/BigPill.swift
@@ -10,32 +10,31 @@ import SwiftUI
 struct BigPill: View {
     @State private var myData = [Int]()
     var body: some View {
-        ZStack {
-            VStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                    .fontWeight(.bold)
-                
-                ForEach(myData, id:\.self) { oneData in
-                    HStack {
-                        Text("\(oneData)")
-                            .font(.footnote)
-                            .lineLimit(2)
-                            .foregroundColor(.primary)
-                            .padding(5)
-                            .background(Color.gray)
-                            .cornerRadius(10)
-                        Spacer()
-                    }
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            ForEach(myData, id:\.self) { oneData in
+                HStack {
+                    Text("\(oneData)")
+                        .font(.footnote)
+                        .lineLimit(2)
+                        .foregroundColor(.white)
+                        .padding(5)
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                    Spacer()
                 }
             }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 25.0))
-            .padding(.horizontal)
         }
+        .padding()
+        .background(Color.blue)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 25.0))
+        .padding(.horizontal)
         .onAppear {
-            for i in 0..<21 {
+            for i in 0..<20 {
                 myData.append(i)
             }
         }
