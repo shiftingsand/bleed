9a4a25ddbbb2618b105fd914001b97abbb7f9b2b
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2021-09-20 09:09:51 -0400

    Added commented out ScrollView to show the behavior.

--- bleed/BigPill.swift
+++ bleed/BigPill.swift
@@ -11,35 +11,38 @@ struct BigPill: View {
     @State private var myData = [Int]()
     var body: some View {
         NavigationView {
-            VStack {
-                Text("I like turtles")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                
-                ForEach(myData, id:\.self) { oneData in
-                    HStack {
-                        Text("\(oneData)")
-                            .font(.footnote)
-                            .lineLimit(2)
-                            .foregroundColor(.white)
-                            .padding(5)
-                            .background(Color.gray)
-                            .cornerRadius(10)
-                        Spacer()
+// Example that shows that without a ScrollView the VStack will bleed into the tab bar.
+//            ScrollView {
+                VStack {
+                    Text("I like turtles")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    
+                    ForEach(myData, id:\.self) { oneData in
+                        HStack {
+                            Text("\(oneData)")
+                                .font(.footnote)
+                                .lineLimit(2)
+                                .foregroundColor(.white)
+                                .padding(5)
+                                .background(Color.gray)
+                                .cornerRadius(10)
+                            Spacer()
+                        }
                     }
                 }
-            }
-            .padding()
-            .background(Color.yellow)
-            .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 25.0))
-            .padding(.horizontal)
-            .navigationTitle("Fancy Title")
-            .onAppear {
-                for i in 0..<17 {
-                    myData.append(i)
+                .padding()
+                .background(Color.yellow)
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                .padding(.horizontal)
+                .navigationTitle("Fancy Title")
+                .onAppear {
+                    for i in 0..<17 {
+                        myData.append(i)
+                    }
                 }
-            }
+//            }
         }
     }
 }
